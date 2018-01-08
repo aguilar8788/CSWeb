12023007ef7c7ec5edf6f4aa85f59f940261c9fe
Peter <peter.aguilar2287@gmail.com>
2018-01-07 23:30:18 -0500

    add background for content

--- src/components/home/component/Home.js
+++ src/components/home/component/Home.js
@@ -74,8 +74,8 @@ class Home extends Component {
             this.setState({
                 candle1: {marginTop: '70px', transition: 'all 1.9s cubic-bezier(.25,.8,.25,1)'},
                 candle2: {marginTop: '120px', transition: 'all 1.9s cubic-bezier(.25,.8,.25,1)', height: '80px'},
-                candle3: {marginTop: '220px', transition: 'all 1.9s cubic-bezier(.25,.8,.25,1)', height: '40px', color: 'red'},
-                candle4: {marginTop: '140px', transition: 'all 1.9s cubic-bezier(.25,.8,.25,1)', height: '40px', color: 'green'},
+                candle3: {marginTop: '220px', transition: 'all 1.9s cubic-bezier(.25,.8,.25,1)', height: '40px', color: '#c43905'},
+                candle4: {marginTop: '140px', transition: 'all 1.9s cubic-bezier(.25,.8,.25,1)', height: '40px', color: '#84BA4D'},
                 logo: {height: (100 + scrollTop) + 'vh', transition: 'all 1.9s cubic-bezier(.25,.8,.25,1)'}
             })
         }
@@ -157,33 +157,36 @@ class Home extends Component {
                     </div>
                 </div>
                 <div className="contentWrapper2">
-                    <h1>What we do</h1>
-                    <p>
-                        Crypto can be a confusing subject to wrap your head around. The Crypto Savages are here to change that.
-                        We take the convoluted subjects that encompass the block chain world, and explain it in a language that the
-                        rest of us understand.
-                    </p>
-                    <div className="subjects">
-                        <div className="content">
-                            <h1>Crypto Trading/Exchanges</h1>
-                            <p>
-                                In depth explanations showing you how easy it is to get started with crypto trading. How is a crypto coin different from a stock
-                                We will get you up and running buying alt coins, and also show how to store them.
-                            </p>
-                        </div>
-                        <div className="content">
-                            <h1>Blockchain Technology</h1>
-                            <p>
-                                What is a blockchain? What significance does it play in cryptocurrency. Smart contracts, P2P, nodes, etc. If any of these words confuse you,
-                                then you come to our channel, and let us clear things up for you.
-                            </p>
-                        </div>
-                        <div className="content">
-                            <h1>Hardware</h1>
-                            <p>
-                                So you can buy crypto, but where do I store them now? What is the difference between a cold wallet, and a hot wallet. Where should I store my crypto once I buy it? We offer reviews on hardware wallets,
-                                exchanges, and wallet programs for your computer and phone.
-                            </p>
+                    <div className="content">
+
+                        <h1>What we do</h1>
+                        <p>
+                            Crypto can be a confusing subject to wrap your head around. The Crypto Savages are here to change that.
+                            We take the convoluted subjects that encompass the block chain world, and explain it in a language that the
+                            rest of us understand.
+                        </p>
+                        <div className="subjects">
+                            <div className="subject">
+                                <h1>Crypto Trading/Exchanges</h1>
+                                <p>
+                                    In depth explanations showing you how easy it is to get started with crypto trading. How is a crypto coin different from a stock
+                                    We will get you up and running buying alt coins, and also show how to store them.
+                                </p>
+                            </div>
+                            <div className="subject">
+                                <h1>Blockchain Technology</h1>
+                                <p>
+                                    What is a blockchain? What significance does it play in cryptocurrency. Smart contracts, P2P, nodes, etc. If any of these words confuse you,
+                                    then you come to our channel, and let us clear things up for you.
+                                </p>
+                            </div>
+                            <div className="subject">
+                                <h1>Hardware</h1>
+                                <p>
+                                    So you can buy crypto, but where do I store them now? What is the difference between a cold wallet, and a hot wallet. Where should I store my crypto once I buy it? We offer reviews on hardware wallets,
+                                    exchanges, and wallet programs for your computer and phone.
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
