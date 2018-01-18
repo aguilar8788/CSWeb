85155e6fba5ec66efde540c544257f3bbe70ea39
Peter <peter.aguilar2287@gmail.com>
2018-01-18 11:08:51 -0500

    add blog section

--- src/components/blog/component/Blog.js
+++ src/components/blog/component/Blog.js
@@ -13,18 +13,47 @@ class Blog extends Component {
         this.props.actions.loadBlogPosts()
     }
 
+    getUrlFromString(body) {
+        var imageUrl = body.match(/\bhttps?:\/\/\S+/gi)
+        console.log("image", imageUrl)
+
+        return imageUrl.map((url) => {
+            if (url.indexOf("steemitimages") >= 0) {
+                return (
+                    <img className="steemImage" src={url} />
+                )
+            }
+        })
+    }
+
+    removeUrlsFromString(body) {
+        let newBodyStr = []
+        let join = ""
+        let replace = ""
+        let splitBody = body.split(" ")
+        splitBody.map((subString, index) => {
+            if (!subString.includes("https://steemitimages.com") && index < 90) {
+                newBodyStr.push(subString)
+            }
+        })
+        join = newBodyStr.join()
+        replace = join.replace(/,/g , " ")
+        return replace
+    }
+
     renderPosts(arrayOfPosts) {
         return arrayOfPosts.map((posts, key) => {
-            console.log(posts, 'osisfa')
+            console.log("posts", posts)
             let title = posts.permlink
             let removeDashes = title.replace(/-/g, ' ')
-
+            let removeImgUrls = this.removeUrlsFromString(posts.body)
             return (
                 <div key={key} className="BlogContainer container-fluid">
                     <div className="blogPost">
-                        <h1>{removeDashes}</h1>
+                        <h1>{removeDashes.toUpperCase()}</h1>
                         <p>by: {posts.author}</p>
-                        <p>{posts.body}</p>
+                        <p>{removeImgUrls}<a href={`https://steemit.com${posts.url}`}> ...Continue reading post</a></p>
+
                     </div>
                 </div>
             )
